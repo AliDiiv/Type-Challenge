34c6bce07fbc577b351e0a51bc7116b5d1dc0bec
Ali Muhammad Jafari <138518459+AliDiiv@users.noreply.github.com>
2025-09-28 11:08:34 +0330

    Initial commit: Typing Challenge Angular App

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
+import { NgIf, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgIf, NgFor],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  randomText: string = faker.lorem.sentence();
+  inputText: string = '';
+
+  onChangeInput(text: string) {
+    this.inputText = text;
+  }
+
+  compare(letterRandom: string, letterEnter: string | undefined): string {
+    if (!letterEnter) return 'pending';
+    return letterEnter === letterRandom ? 'correct' : 'incorrect';
+  }
+
+  get progress(): number {
+    if (!this.randomText) return 0;
+    const correctChars = this.inputText
+      .split('')
+      .filter((ch, i) => ch === this.randomText[i]).length;
+
+    return Math.floor((correctChars / this.randomText.length) * 100);
+  }
+}
